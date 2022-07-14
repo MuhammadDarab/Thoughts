63c89c9daf75add2d92c1f5ae703625cf1304070
MuhammadDarab <darabmonib123@gmail.com>
2022-07-14 07:39:43 +0500

    callback URL..

--- server/pass.js
+++ server/pass.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(
     new GoogleStrategy({
-        callbackURL:'/auth/google/redirect',
+        callbackURL:'https://thoughtsbackend.vercel.app/auth/google/redirect',
         clientID:'367258283721-ppbo9e6g2o009dg6odvnpvnaerfqq0ki.apps.googleusercontent.com',
         clientSecret:'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'
     }, (accessToken, refreshToken, profile, done) => {
