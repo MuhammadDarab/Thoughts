8b8ec362e265cf57dd28a926c8e9c57334a56859
MuhammadDarab <darabmonib123@gmail.com>
2022-07-17 11:16:34 +0500

    hard-coded reroute

--- app/src/comps/login.js
+++ app/src/comps/login.js
@@ -15,7 +15,7 @@ const Login = () => {
 
         <div className="cursor-pointer my-8 flex items-center rounded-2xl shadow-xl border-blue-400 text-blue-400 border-2 text-center w-fit mx-auto p-4 hover:transition-all transition-all hover:shadow-2xl"
             onClick={() => {
-                window.open(env.backendURL + '/auth/google/redirect', '_self')
+                window.open('https://thoughtsbackend.vercel.app/auth/google/redirect', '_self')
             }}
         >
             <img width='40px' height='auto' src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png' alt='google'/>
